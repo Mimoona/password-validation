ed0d489cf8c4592c8b058f39897bda9ec7f9e883
Mimoona <memoona.raheel8@gmail.com>
2025-03-31 15:40:33 +0200

    implement functionality in all methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,21 +16,47 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(isCommonPassword("123pass"));
     }
 
     public static boolean isLengthValid(String password){
-        return true;
+        return password.length() >= 8;
     }
 
     public static boolean containsNumbers(String password){
-        return true;
+        for(int i=0; i < password.length(); i++){
+            if (Character.isDigit(password.charAt(i))){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static boolean containsUpperAndLowercase(String password){
-        return true;
+        boolean hasLowerCase = false;
+        boolean hasUpperCase = false;
+
+        for(char c : password.toCharArray()){
+
+            if(Character.isLowerCase(c)){
+                hasLowerCase = true;
+            }
+
+            if(Character.isUpperCase(c)){
+                hasUpperCase = true;
+            }
+        }
+
+        return hasLowerCase && hasUpperCase;
     }
 
     public static boolean isCommonPassword(String password){
+        String[] commonPasswords = {"1234abcd", "password1", "Aa345678", "123pass"};
+        for(String commonPassword: commonPasswords){
+            if(commonPassword.equals(password)){
+                return true;
+            }
+        }
         return false;
     }
 }
